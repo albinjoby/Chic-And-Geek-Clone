09f68c669fae6aa24a1bc44fcce4760f63252c67
Albin Joby <albinjobyponnu@gmail.com>
2025-03-24 23:25:42 +0530

    initial

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,66 @@
 import Image from "next/image";
+import localFont from "next/font/local";
+
+const apple = localFont({
+  src: "../../public/fonts/AppleGaramond-Light.ttf",
+  variable: "--font-apple",
+});
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className={`${apple.className}`}>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 relative overflow-hidden mb-10">
+        <div>
+          <h1 className="text-9xl p-20">
+            <span className="block">Chic. </span>And Geek
+          </h1>
+          <p className="text-4xl p-10 pt-0">
+            I am Nenad Grujicic, a multidisciplinary designer experienced in
+            making brands and GUls look good, feel great, and work well.
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="relative h-full">
+          <div className="lg:absolute lg:right-0 lg:bottom-0">
+            <Image
+              className="w-full h-auto object-cover"
+              src="/images/img1.jpg"
+              width={700}
+              height={500}
+              alt="Image description"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 relative overflow-hidden">
+        <div className="p-10">
+          <div className="p-12 pb-0 pt-20 bg-gray-100 rounded-4xl">
+            <Image
+              className="bottom-0"
+              src="/images/img2.gif"
+              width={600}
+              height={500}
+              alt="Image description"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <p className="text-4xl p-10 pt-0 lg:pt-15 w-[90%] lg:w-[100%]">
+            At the moment, I am crafting eye- catching, user-loved things at
+            Pudding Studio.
+          </p>
+          <p className="text-xl p-10 pt-0 w-[90%] lg:w-[100%]">
+            Pudding is a small, family-run studio. It is formed by two of us, my
+            spouse Jelena and me. The primary focus for our studio is close
+            collaboration with product startups and their progressive growth.
+          </p>
+        </div>
+      </div>
+      <div className="text-center p-5 lg:p-45 mt-30">
+        <h2 className="text-7xl">
+          I helped TextNow establish its brand presence while making it widely
+          recognizable.
+        </h2>
+      </div>
     </div>
   );
 }
